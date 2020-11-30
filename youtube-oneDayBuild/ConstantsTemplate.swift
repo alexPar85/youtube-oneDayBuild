4f47917db8bc0d21aeec0660048cf957dcc8abc2
mac <alextheripper@hotmail.com>
2020-11-30 20:11:09 +0200

    modified .gitignore addded cosntantsTemplate.swift

---
+++ youtube-oneDayBuild/ConstantsTemplate.swift
@@ -0,0 +1,19 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-oneDayBuild
+//
+//  Created by mac on 30/11/20.
+//
+// Add your own keys below and uncomment the code
+import Foundation
+
+/*
+ 
+ struct Constants {
+     
+     static var API_KEY = ""
+     static var PLAYLIST_ID = ""
+     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+ }
+
+ */
---
+++ youtube-oneDayBuild/Video.swift
@@ -0,0 +1,16 @@
+//
+//  Video.swift
+//  youtube-oneDayBuild
+//
+//  Created by mac on 30/11/20.
+//
+
+import Foundation
+
+struct Video{
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
